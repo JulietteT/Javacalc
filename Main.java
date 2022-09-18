ea4cc141b67709e190336ae50c720e9c1a1587a8
Юлия Ц <107360558+JulietteT@users.noreply.github.com>
2022-09-18 14:25:54 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,3 +8,7 @@ public class Main {
 
     }
 }
+C:\Users\user\.jdks\openjdk-18.0.2.1\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.2.2\lib\idea_rt.jar=51281:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.2.2\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath C:\Users\user\IdeaProjects\Javacalc\out\production\Javacalc Main
+Бонусные мили:700
+
+Process finished with exit code 0
