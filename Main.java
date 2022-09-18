5b21394fba80757a6bde8305c947d0e294a56f19
JulietteT <Ihbrbh19@mail.ru>
2022-09-18 13:51:49 +0500

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int price = 14_000;
+
+        int miles = price / 20;
+        System.out.println("Бонусные мили " + miles);
+
+    }
+}
